91244c1b601e5bc111ad5d4e5dfa78b4fa0eb39d
VM178 <vedantmadhe18@gmail.com>
2023-09-16 17:47:24 +0530

    Right eye view of binary tree.

---
+++ Binary_Tree_Right_Side_View.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void rtol(TreeNode* root,int l,vector<int> &v){
+        if(root == NULL){
+            return;
+        }
+        if(v.size() == l){
+            v.push_back(root->val);
+        }
+        rtol(root->right,l+1,v);
+        rtol(root->left,l+1,v);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        rtol(root,0,ans);
+        return ans;
+    }
+};
+
